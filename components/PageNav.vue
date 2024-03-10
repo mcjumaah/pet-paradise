e679eb94f4f7671083b23cd3b325a8c8b651e5e3
mcjumaah <87046678+mcjumaah@users.noreply.github.com>
2024-03-10 18:43:57 +0800

    refactor: Moved page navigation links to new separate component

---
+++ components/PageNav.vue
@@ -0,0 +1,27 @@
+<template>
+	<menu class="nav-pages d-flex gap-5 poppins-medium ps-0">
+		<NuxtLink to="/">Home</NuxtLink>
+		<NuxtLink to="/shop">Shop</NuxtLink>
+		<NuxtLink to="/about">About</NuxtLink>
+		<NuxtLink to="/contact">Contact</NuxtLink>
+	</menu>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+menu.nav-pages {
+	:deep(a) {
+		color: rgba(0, 0, 0, 0.75);
+		text-decoration: none;
+		&:hover {
+			text-decoration: underline;
+			color: var(--bs-primary-text-emphasis);
+		}
+	}
+
+	.router-link-exact-active {
+		color: var(--bs-primary);
+	}
+}
+</style>
