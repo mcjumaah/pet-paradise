12150f80c884409845a2ddabdd73afda7374eaab
mcjumaah <87046678+mcjumaah@users.noreply.github.com>
2024-03-12 17:42:42 +0800

    feat: Created Signup page

---
+++ pages/signup.vue
@@ -0,0 +1,23 @@
+<template>
+	<div class="login-form d-flex flex-column row-gap-4">
+		<input type="text" class="form-control" placeholder="Email Address" aria-label="Email Address" />
+
+		<button class="btn btn-primary w-100 text-white">NEXT</button>
+	</div>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+	layout: "login-signup",
+});
+</script>
+
+<style scoped lang="scss">
+.login-form {
+	input.form-control,
+	button {
+		font-size: 14px;
+		height: 3.188rem;
+	}
+}
+</style>
