c3da50d1a9256e7ef043a1d11257f2839af9a933
mcjumaah <87046678+mcjumaah@users.noreply.github.com>
2024-03-16 18:32:35 +0800

    feat(About page): Added card component for 'Who We Serve' section

---
+++ components/WhoWeServeCard.vue
@@ -0,0 +1,58 @@
+<template>
+	<div class="who-we-serve-card card shadow rounded">
+		<div class="grid flex-column h-100">
+			<div class="card-img g-col-6">
+				<img :src="props.imgSrc" class="object-fit-cover rounded-start w-100 h-100" alt="CARD IMAGE" />
+			</div>
+
+			<div
+				class="card-body g-col-6 text-center bg-body-2 d-flex flex-column align-items-center justify-content-center h-100"
+			>
+				<h4 class="card-title poppins-bold">{{ props.title }}</h4>
+				<p class="card-text text-muted poppins-medium">
+					<slot />
+				</p>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script setup lang="ts">
+export interface Props {
+	imgSrc?: string;
+	title?: string;
+}
+const props = withDefaults(defineProps<Props>(), {
+	title: "TITLE HERE",
+});
+</script>
+
+<style scoped lagn="scss">
+.who-we-serve-card.card {
+	max-width: 27.5rem;
+	overflow: clip;
+
+	@media (min-width: 768px) {
+		max-width: 17.5rem;
+	}
+	@media (min-width: 992px) {
+		max-width: 27.5rem;
+	}
+	@media (min-width: 1200px) {
+		max-width: 31.5rem;
+	}
+
+	.grid {
+		--bs-gap: 0;
+
+		display: grid;
+
+		@media (min-width: 768px) {
+			display: flex;
+		}
+		@media (min-width: 992px) {
+			display: grid;
+		}
+	}
+}
+</style>
