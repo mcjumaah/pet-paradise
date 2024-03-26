2316a130aa474325de19306b731ef00548fa8f8c
mcjumaah <87046678+mcjumaah@users.noreply.github.com>
2024-03-26 21:30:34 +0800

    feat: Added `@vuepic/vue-datepicker` as plugin

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	devtools: { enabled: true },
+	devtools: {
+		enabled: false,
+
+		timeline: {
+			enabled: false,
+		},
+	},
 	css: ["~/assets/styles/main.scss"],
-	plugins: ["~/plugins/useBoootstrap.client.js"],
+	plugins: ["~/plugins/useBoootstrap.client.js", "~/plugins/useVueDatePicker.ts"],
 });
