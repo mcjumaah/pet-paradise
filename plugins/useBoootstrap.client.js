acd291537a1317596544e7df7b007a686913af5f
mcjumaah <87046678+mcjumaah@users.noreply.github.com>
2024-03-28 16:56:29 +0800

    build(Bootstrap): Added Collapse and Offcanvas to mixins

--- plugins/useBoootstrap.client.js
+++ plugins/useBoootstrap.client.js
@@ -1,5 +1,7 @@
-import { Dropdown } from "bootstrap";
+import { Dropdown, Collapse, Offcanvas } from "bootstrap";
 
 export default defineNuxtPlugin((nuxtApp) => {
 	nuxtApp.vueApp.mixin(Dropdown);
+	nuxtApp.vueApp.mixin(Collapse);
+	nuxtApp.vueApp.mixin(Offcanvas);
 });
