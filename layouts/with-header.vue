a289a9c8013490365699242e57e2ab12371a192d
mcjumaah <87046678+mcjumaah@users.noreply.github.com>
2024-03-29 13:07:50 +0800

    fix(`with-header` layout): Resolved path shown in header section for longer paths

--- layouts/with-header.vue
+++ layouts/with-header.vue
@@ -5,8 +5,16 @@
 				<div class="position-relative d-flex flex-column align-items-center z-1 text-white">
 					<h2>{{ basePathTitle }}</h2>
 					<Logo v-if="basePathTitle == 'About'" class="pt-3 pb-4" :isXl="true" />
-					<p>
-						<NuxtLink to="/">Home</NuxtLink> > <span class="poppins-bold">{{ basePathTitle }}</span>
+					<p class="d-flex column-gap-2">
+						<NuxtLink to="/">Home</NuxtLink> >
+						<template v-for="(path, index) in pathArr">
+							<span v-if="getIsNotLastPathArrSegment(index)" class="d-flex column-gap-2">
+								<NuxtLink :to="getPathPerSegment(index)">{{ useRoutePathTitle(index).value }}</NuxtLink> >
+							</span>
+							<span v-else class="poppins-bold">
+								{{ useRoutePathTitle(index).value }}
+							</span>
+						</template>
 					</p>
 				</div>
 				<img :src="currentBgImg" class="position-absolute top-0 w-100 h-100 object-fit-cover opacity-25 z-0 bg-white" />
@@ -24,11 +32,24 @@ const bgImages = [
 	{ basePathTitle: "Account", src: "/images/header-bg/account.png" },
 ];
 
+const pathArr = useRoutePathArr();
 const basePathTitle = useRoutePathTitle();
+
 const currentBgImg = computed(() => {
 	let computedObject = bgImages.find((obj) => obj.basePathTitle == basePathTitle.value);
 	return computedObject?.src ? computedObject.src : "/images/header-bg/shop.png";
 });
+
+function getIsNotLastPathArrSegment(index: number) {
+	return pathArr.value.length > 0 && pathArr.value.length !== index + 1;
+}
+function getPathPerSegment(currentIndex: number) {
+	let path = "";
+	for (let i = 0; i <= currentIndex; i++) {
+		path = `${path}/${pathArr.value[i]}`;
+	}
+	return path;
+}
 </script>
 
 <style scoped lang="scss">
