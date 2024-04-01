87dec592d99b9dc1c118650dd4fc89d8d81d3682
mcjumaah <87046678+mcjumaah@users.noreply.github.com>
2024-04-01 21:18:21 +0800

    feat(Bootstrap): Added Tooltip in plugin

--- plugins/useBoootstrap.client.js
+++ plugins/useBoootstrap.client.js
@@ -1,7 +1,14 @@
-import { Dropdown, Collapse, Offcanvas } from "bootstrap";
+import { Dropdown, Collapse, Offcanvas, Tooltip } from "bootstrap";
 
 export default defineNuxtPlugin((nuxtApp) => {
 	nuxtApp.vueApp.mixin(Dropdown);
 	nuxtApp.vueApp.mixin(Collapse);
 	nuxtApp.vueApp.mixin(Offcanvas);
+	nuxtApp.vueApp.mixin(Tooltip);
+
+	return {
+		provide: {
+			Tooltip,
+		},
+	};
 });
