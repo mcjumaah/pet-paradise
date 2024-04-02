abd2aa02271208cc05a294758cd7d2cefa62af46
mcjumaah <87046678+mcjumaah@users.noreply.github.com>
2024-04-02 21:04:14 +0800

    feat: Added composable for dummy products

---
+++ composables/dummyProducts.ts
@@ -0,0 +1,94 @@
+export const useDummyProducts = () => {
+	return useState("dummy-products", () => [
+		{
+			id: 1,
+			name: "Dr Shiba Happy Tummy Healthy Dog Treats Snack - Dog Multivitamins - Dog Food for Digestive Care",
+			price: "₱499",
+			previewImg: "/images/shop-products/01.png",
+		},
+		{
+			id: 2,
+			name: "PEDIGREE DentaStix for Puppy – Dental Treats for Puppy, 56g. Daily Puppy Treats for Healthy Gums",
+			price: "₱71",
+			previewImg: "/images/shop-products/02.png",
+		},
+		{
+			id: 3,
+			name: "Dr. Shiba Happy Tummy Taster Pack - Beef 40g  Dog Treats Snack - Dog Food for Digestive Care",
+			price: "₱99",
+			previewImg: "/images/shop-products/03.png",
+		},
+		{
+			id: 4,
+			name: "Mandaue Foam Dog Bed",
+			price: "₱735",
+			previewImg: "/images/shop-products/04.png",
+		},
+		{
+			id: 5,
+			name: "BATTERY CAGE (3 DOOR/4 DOOR/5 DOOR) - Mr.Chuck",
+			price: "₱990 - ₱1,290",
+			previewImg: "/images/shop-products/05.png",
+		},
+		{
+			id: 6,
+			name: "Rojeco 3.2L Cat Drinking Fountain Filters 304 Stainless Steel Pet Fountains Filter Replacement",
+			price: "₱329 - ₱629",
+			previewImg: "/images/shop-products/06.png",
+		},
+		{
+			id: 7,
+			name: "Petcher Detick and Alprocide 1cc & 2cc with Free Syringe Anti Tick and Flea Spot on Treatment",
+			price: "₱37 - ₱92",
+			previewImg: "/images/shop-products/07.png",
+		},
+		{
+			id: 8,
+			name: "Alonefire 9LED 395nm Aluminum Alloy Mini UV Flashlight Travel Cat Dog Urine Money Detector Torch For AAA Battery",
+			price: "₱75 - ₱79",
+			previewImg: "/images/shop-products/08.png",
+		},
+		{
+			id: 9,
+			name: "Rojeco Cat Water Fountain Replaceable Activated Carbon Filters Filter For Pet Automatic Drinking",
+			price: "₱179 - ₱529",
+			previewImg: "/images/shop-products/09.png",
+		},
+		{
+			id: 10,
+			name: "Alonefire 9LED 395nm Aluminum Alloy Mini UV Flashlight Travel Cat Dog Urine Money Detector Torch For AAA Battery",
+			price: "₱859",
+			previewImg: "/images/shop-products/10.png",
+		},
+		{
+			id: 11,
+			name: "Alonefire 9LED 395nm Aluminum Alloy Mini UV Flashlight Travel Cat Dog Urine Money Detector Torch For AAA Battery",
+			price: "₱859",
+			previewImg: "/images/shop-products/11.png",
+		},
+		{
+			id: 12,
+			name: "Alonefire 9LED 395nm Aluminum Alloy Mini UV Flashlight Travel Cat Dog Urine Money Detector Torch For AAA Battery",
+			price: "₱859",
+			previewImg: "/images/shop-products/12.png",
+		},
+		{
+			id: 13,
+			name: "Alonefire 9LED 395nm Aluminum Alloy Mini UV Flashlight Travel Cat Dog Urine Money Detector Torch For AAA Battery",
+			price: "₱859",
+			previewImg: "/images/shop-products/13.png",
+		},
+		{
+			id: 14,
+			name: "Alonefire 9LED 395nm Aluminum Alloy Mini UV Flashlight Travel Cat Dog Urine Money Detector Torch For AAA Battery",
+			price: "₱859",
+			previewImg: "/images/shop-products/14.png",
+		},
+		{
+			id: 15,
+			name: "Alonefire 9LED 395nm Aluminum Alloy Mini UV Flashlight Travel Cat Dog Urine Money Detector Torch For AAA Battery",
+			price: "₱859",
+			previewImg: "/images/shop-products/15.png",
+		},
+	]);
+};
