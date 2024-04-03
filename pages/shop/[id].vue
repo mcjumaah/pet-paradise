ddbae4e65641e0997801cfa5f95960283038d8b4
mcjumaah <87046678+mcjumaah@users.noreply.github.com>
2024-04-03 13:06:59 +0800

    feat(Product page): Configured page meta title

---
+++ pages/shop/[id].vue
@@ -0,0 +1,33 @@
+<template>
+	<div class="product-page">Product Page</div>
+</template>
+
+<script setup lang="ts">
+const route = useRoute();
+
+const product = useDummyProducts().value.find((product) => {
+	if (product.id === parseInt(route.params.id as string)) {
+		return product;
+	}
+});
+
+const pageTitle = computed(() => {
+	let maxLength = 10;
+	let title: string;
+
+	if (product?.name) {
+		title = product?.name.length > maxLength ? product?.name.slice(0, length - 3) + "..." : product?.name;
+	} else {
+		title = "Product";
+	}
+
+	return `${title} – Pet Paradise`;
+});
+
+useSeoMeta({
+	title: pageTitle,
+	ogTitle: pageTitle,
+});
+</script>
+
+<style scoped></style>
