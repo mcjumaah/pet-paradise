ca610cf959755ce5b88ce2c450398d30e46ac525
mcjumaah <87046678+mcjumaah@users.noreply.github.com>
2024-04-07 14:28:41 +0800

    build(`useBoootstrap` plugin): Converted to Typescript from Javascript

--- plugins/useBoootstrap.client.ts
+++ plugins/useBoootstrap.client.ts
@@ -1,10 +1,10 @@
 import { Dropdown, Collapse, Offcanvas, Tooltip } from "bootstrap";
 
 export default defineNuxtPlugin((nuxtApp) => {
-	nuxtApp.vueApp.mixin(Dropdown);
-	nuxtApp.vueApp.mixin(Collapse);
-	nuxtApp.vueApp.mixin(Offcanvas);
-	nuxtApp.vueApp.mixin(Tooltip);
+	nuxtApp.vueApp.mixin(Dropdown as any);
+	nuxtApp.vueApp.mixin(Collapse as any);
+	nuxtApp.vueApp.mixin(Offcanvas as any);
+	nuxtApp.vueApp.mixin(Tooltip as any);
 
 	return {
 		provide: {
