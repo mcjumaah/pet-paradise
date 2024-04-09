ff3a60ae5a797c64dddbfe5b719848f1d954a3a6
mcjumaah <87046678+mcjumaah@users.noreply.github.com>
2024-04-09 13:03:41 +0800

    feat(`selection` entity): Added base queries

--- server/model/selection.ts
+++ server/model/selection.ts
@@ -1,6 +1,81 @@
 import { sql } from "../db";
 
 export type ProductSelection = {
+	id: number;
 	name: string;
-	varieties: string[];
+	productId: number;
+};
+
+export const findAll = async () => {
+	try {
+		const result = await sql({ query: `SELECT * FROM selection` });
+
+		return result as ProductSelection[];
+	} catch (error) {
+		return error;
+	}
+};
+
+export const findById = async (id: string) => {
+	try {
+		const result = (await sql({
+			query: `SELECT * FROM selection WHERE id = ?`,
+			values: [id],
+		})) as ProductSelection[];
+
+		return result.length === 1 ? result[0] : null;
+	} catch (error) {
+		return error;
+	}
+};
+
+export const save = async (data: Pick<ProductSelection, "name" | "productId">) => {
+	try {
+		const result = (await sql({
+			query: `
+        INSERT INTO selection (
+          name, 
+					product_id
+        ) VALUES (?, ?) 
+        RETURNING *
+			`,
+			values: [data.name, data.productId],
+		})) as ProductSelection[];
+
+		return result.length === 1 ? result[0] : null;
+	} catch (error) {
+		return error;
+	}
+};
+
+export const update = async (id: string, data: Pick<ProductSelection, "name" | "productId">) => {
+	try {
+		await sql({
+			query: `
+				UPDATE selection 
+				SET 
+					name = ?, 
+					product_id = ?, 
+				WHERE id = ?
+			`,
+			values: [data.name, data.productId, id],
+		});
+
+		return await findById(id);
+	} catch (error) {
+		return error;
+	}
+};
+
+export const deleteById = async (id: string) => {
+	try {
+		await sql({
+			query: `DELETE FROM selection WHERE id = ?`,
+			values: [id],
+		});
+
+		return true;
+	} catch (error) {
+		return error;
+	}
 };
