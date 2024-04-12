f5bc484f0eafc177b7420934a383c9f747facf00
mcjumaah <87046678+mcjumaah@users.noreply.github.com>
2024-04-12 17:04:43 +0800

    feat(Login page): Integrated login functionality

--- pages/login.vue
+++ pages/login.vue
@@ -1,15 +1,58 @@
 <template>
-	<div class="login-form d-flex flex-column row-gap-4">
-		<input type="text" class="form-control" placeholder="Email Address" aria-label="Email Address" />
-		<input type="text" class="form-control" placeholder="Password" aria-label="Password" />
+	<form id="login-form" class="login-form d-flex flex-column row-gap-4" @submit.prevent="handleSignIn()">
+		<div class="form-floating">
+			<input
+				id="email-input"
+				class="form-control"
+				name="email"
+				type="email"
+				placeholder="name@example.com"
+				aria-label="Email Address"
+			/>
+			<label for="email-input" class="text-muted">Email address</label>
+		</div>
+		<div class="form-floating">
+			<input
+				id="password-input"
+				class="form-control"
+				name="password"
+				type="password"
+				placeholder="Password"
+				aria-label="Password"
+			/>
+			<label for="password-input" class="text-muted">Password</label>
+		</div>
+
 		<div class="login-btn-wrapper d-flex flex-column">
-			<button class="btn btn-primary w-100 text-white">LOG IN</button>
+			<button type="submit" class="btn btn-primary w-100 text-white">LOG IN</button>
 			<NuxtLink to="/forgot-password" class="link-secondary-sm">Forgot Password?</NuxtLink>
 		</div>
-	</div>
+	</form>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+definePageMeta({
+	middleware: "auth",
+	auth: {
+		unauthenticatedonly: true,
+		navigateauthenticatedto: "/",
+	},
+});
+
+const { signIn } = useAuth();
+
+function handleSignIn() {
+	const loginFormEl = document.getElementById("login-form") as HTMLFormElement;
+	const emailInput = loginFormEl?.elements.namedItem("email") as HTMLInputElement;
+	const passwordInput = loginFormEl?.elements.namedItem("password") as HTMLInputElement;
+	const emailVal = emailInput.value;
+	const passwordVal = passwordInput.value;
+
+	if (emailVal && passwordVal) {
+		signIn("credentials", { emailVal, passwordVal });
+	}
+}
+</script>
 
 <style scoped lang="scss">
 .login-form {
