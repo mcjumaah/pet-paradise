82ca68511d18e9320f1b2d94f79004c72db29f2b
mcjumaah <87046678+mcjumaah@users.noreply.github.com>
2024-04-12 15:28:27 +0800

    feat(Login endpoint): Added auth

---
+++ server/api/auth/[...].ts
@@ -0,0 +1,25 @@
+// file: ~/server/api/auth/[...].ts
+import { NuxtAuthHandler } from "#auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import * as customerModel from "../../model/costumer";
+
+export default NuxtAuthHandler({
+	providers: [
+		// @ts-expect-error You need to use .default here for it to work during SSR. May be fixed via Vite at some point
+		CredentialsProvider.default({
+			name: "Credentials",
+			async authorize(credentials: any) {
+				const result = (await customerModel.findOneByCredential({
+					email: credentials?.email,
+					password: credentials?.password,
+				})) as customerModel.Customer;
+
+				if (result.id) {
+					return result;
+				} else {
+					return { error: "Sign in failed. Check the details you provided are correct" };
+				}
+			},
+		}),
+	],
+});
