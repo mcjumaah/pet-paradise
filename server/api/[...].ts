d954aef7e9ca359e4fbadc78975b89c441ef3979
mcjumaah <87046678+mcjumaah@users.noreply.github.com>
2024-04-16 13:54:55 +0800

    feat(API Endpoints): Added `api/products`

--- server/api/[...].ts
+++ server/api/[...].ts
@@ -4,6 +4,7 @@ import * as productCtrl from "../controller/productController";
 const router = createRouter();
 
 router.get("/product", defineEventHandler(productCtrl.find));
+router.get("/products", defineEventHandler(productCtrl.find));
 router.post("/product", defineEventHandler(productCtrl.save));
 router.put("/product", defineEventHandler(productCtrl.update));
 router.delete("/product", defineEventHandler(productCtrl.deleteById));
