c102ff097a339b99a4641c34953d4e9965b0efce
mcjumaah <87046678+mcjumaah@users.noreply.github.com>
2024-04-16 14:55:51 +0800

    feat(API endpoints): Added endpoint for saving a customer

--- server/api/[...].ts
+++ server/api/[...].ts
@@ -1,5 +1,6 @@
 import { useBase, createRouter, defineEventHandler } from "#imports";
 import * as productCtrl from "../controller/productController";
+import * as customerCtrl from "../controller/customerController";
 
 const router = createRouter();
 
@@ -8,5 +9,7 @@ router.get("/product", defineEventHandler(productCtrl.findOne));
 router.post("/product", defineEventHandler(productCtrl.save));
 router.put("/product", defineEventHandler(productCtrl.update));
 router.delete("/product", defineEventHandler(productCtrl.deleteOne));
+
+router.post("/customer", defineEventHandler(customerCtrl.save));
 
 export default useBase("/api", router.handler);
