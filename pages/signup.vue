afdd5e4624e8aa5b28f9a01dc2b20ca9b50e7f15
mcjumaah <87046678+mcjumaah@users.noreply.github.com>
2024-04-17 11:30:52 +0800

    feat(Signup page): Added email validation

--- pages/signup.vue
+++ pages/signup.vue
@@ -1,5 +1,5 @@
 <template>
-	<form id="signup-form" class="login-form d-flex flex-column row-gap-4" autocomplete="off" @submit.prevent="">
+	<form id="signup-form" class="login-form d-flex flex-column row-gap-4" @submit.prevent="handleSignUp()" novalidate>
 		<input type="email" style="display: none" />
 		<input type="password" style="display: none" />
 
@@ -7,62 +7,148 @@
 			<input
 				id="signup-email-input"
 				class="form-control"
+				:class="emailInputValidationClass"
 				name="email"
 				type="email"
 				placeholder="name@example.com"
 				aria-label="Email Address"
 				autocomplete="off"
+				required
+				v-model="email"
 			/>
 			<label for="signup-email-input" class="text-muted">Email address</label>
+			<div class="invalid-feedback">
+				{{
+					isStringValidEmail
+						? "An account associated with this email address already exists."
+						: "Please input a valid email address."
+				}}
+			</div>
 		</div>
 
-		<div class="form-floating">
-			<input
-				id="create-password-input"
-				class="form-control"
-				name="create-password"
-				type="password"
-				placeholder="Create Password"
-				aria-label="Password"
-			/>
-			<label for="login-password-input" class="text-muted">Create Password</label>
-		</div>
-		<div class="form-floating">
-			<input
-				id="confirm-password-input"
-				class="form-control"
-				name="confirm-password"
-				type="password"
-				placeholder="Confirm Password"
-				aria-label="Password"
-			/>
-			<label for="login-password-input" class="text-muted">Confirm Password</label>
-		</div>
+		<template v-if="isEmailValid">
+			<div class="form-floating">
+				<input
+					id="create-password-input"
+					class="form-control"
+					name="create-password"
+					type="password"
+					placeholder="Create Password"
+					aria-label="Password"
+					required
+				/>
+				<label for="login-password-input" class="text-muted">Create Password</label>
+			</div>
+			<div class="form-floating">
+				<input
+					id="confirm-password-input"
+					class="form-control"
+					name="confirm-password"
+					type="password"
+					placeholder="Confirm Password"
+					aria-label="Password"
+					required
+				/>
+				<label for="login-password-input" class="text-muted">Confirm Password</label>
+			</div>
+		</template>
 
-		<button type="submit" class="btn btn-primary w-100 text-white">NEXT</button>
+		<div
+			id="signup-next-btn-wrapper"
+			data-bs-toggle="tooltip"
+			data-bs-title="Input email first."
+			data-bs-trigger="hover"
+			data-bs-placement="bottom"
+			data-bs-offset="0,10"
+			tabindex="0"
+		>
+			<button type="submit" class="btn btn-primary w-100 text-white" :disabled="isEmailInputEmpty">NEXT</button>
+		</div>
 	</form>
 </template>
 
 <script setup lang="ts">
+import type { Customer } from "~/server/model/customer";
+
 definePageMeta({
 	auth: false,
 });
 
+const { $Tooltip: Tooltip } = useNuxtApp();
+
 const email = ref<string>();
 const createdPassword = ref<string>();
 const confirmedPassword = ref<string>();
+const isEmailValid = ref<boolean>();
+const nextBtnTooltip = ref<typeof Tooltip.prototype>();
 
-function handleSignUp() {
-	const signupFormEl = document.getElementById("signup-form") as HTMLFormElement;
+const isEmailInputEmpty = computed(() => {
+	if (typeof email.value !== "undefined" && email.value?.length > 0) {
+		if (nextBtnTooltip.value) {
+			nextBtnTooltip.value.disable();
+		}
+		return false;
+	} else {
+		if (nextBtnTooltip.value) {
+			nextBtnTooltip.value.enable();
+		}
+		return true;
+	}
+});
 
-	const emailInput = signupFormEl?.elements.namedItem("email") as HTMLInputElement;
-	const createPasswordInput = signupFormEl?.elements.namedItem("create-password") as HTMLInputElement;
-	const confirmPasswordInput = signupFormEl?.elements.namedItem("confirm-password") as HTMLInputElement;
+const isStringValidEmail = computed(() => {
+	let pattern = /^[^\s@]+@[^\s@]+\.com$/;
 
-	email.value = emailInput.value;
-	createdPassword.value = createPasswordInput.value;
-	confirmedPassword.value = confirmPasswordInput.value;
+	if (email.value) {
+		return pattern.test(email.value);
+	} else {
+		return undefined;
+	}
+});
+
+watch(email, () => {
+	if (typeof isEmailValid.value !== "undefined") {
+		isEmailValid.value = undefined;
+	}
+});
+
+const emailInputValidationClass = computed(() => {
+	return isEmailValid.value === true
+		? "is-valid"
+		: isEmailValid.value === false && typeof email.value !== "undefined" && email.value?.length > 0
+		? "is-invalid "
+		: "";
+});
+
+async function handleSignUp() {
+	if (email.value && isStringValidEmail.value) {
+		isEmailValid.value = (await fetchCustomerByEmail(email.value))?.data ? false : true;
+	} else {
+		isEmailValid.value = false;
+	}
+
+	if (isEmailValid.value) {
+	}
 }
+
+async function fetchCustomerByEmail(email: string) {
+	try {
+		const result: { data: Customer } = await $fetch("/api/customer", {
+			method: "GET",
+			query: {
+				email,
+			},
+		});
+
+		return result;
+	} catch (error) {
+		alert(error);
+	}
+}
+
+onMounted(() => {
+	nextBtnTooltip.value = Tooltip.getOrCreateInstance("#signup-next-btn-wrapper");
+});
 </script>
 
 <style scoped lang="scss">
