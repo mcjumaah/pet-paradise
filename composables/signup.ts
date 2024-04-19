bdbcf2aadb079c4c5dabe669a6d4806b62924672
mcjumaah <87046678+mcjumaah@users.noreply.github.com>
2024-04-19 14:58:24 +0800

    Update signup.ts

--- composables/signup.ts
+++ composables/signup.ts
@@ -1,5 +1,5 @@
 export const useSignupCredentials = () => {
 	return useState<{ email: string; password: string }>("signup-credentials", () => {
-		return { email: "test@mail.com", password: "test123" };
+		return { email: "test@mail.com", password: "StrongPass123!" };
 	});
 };
