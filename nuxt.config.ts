e0af233bbdda4ca808e5d6999d0bcfeeb1c9b457
mcjumaah <87046678+mcjumaah@users.noreply.github.com>
2024-04-19 15:42:28 +0800

    build: Installed `nuxt-lodash`

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,7 +7,10 @@ export default defineNuxtConfig({
 			enabled: false,
 		},
 	},
-	modules: ["@sidebase/nuxt-auth"],
+	modules: ["@sidebase/nuxt-auth", "nuxt-lodash"],
 	auth: { globalAppMiddleware: true, provider: { type: "authjs" } },
+	lodash: {
+		prefix: "_",
+	},
 	css: ["~/assets/styles/main.scss"],
 });
