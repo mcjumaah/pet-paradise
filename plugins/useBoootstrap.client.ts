34d85db729f640672dd1eb7def05800fe58411f6
mcjumaah <87046678+mcjumaah@users.noreply.github.com>
2024-04-21 16:40:56 +0800

    build(`useBoootstrap` plugin): Added `Toast` & `Alert`

--- plugins/useBoootstrap.client.ts
+++ plugins/useBoootstrap.client.ts
@@ -1,14 +1,17 @@
-import { Dropdown, Collapse, Offcanvas, Tooltip } from "bootstrap";
+import { Dropdown, Collapse, Offcanvas, Tooltip, Alert, Toast } from "bootstrap";
 
 export default defineNuxtPlugin((nuxtApp) => {
 	nuxtApp.vueApp.mixin(Dropdown as any);
 	nuxtApp.vueApp.mixin(Collapse as any);
 	nuxtApp.vueApp.mixin(Offcanvas as any);
 	nuxtApp.vueApp.mixin(Tooltip as any);
+	nuxtApp.vueApp.mixin(Toast as any);
+	nuxtApp.vueApp.mixin(Alert as any);
 
 	return {
 		provide: {
 			Tooltip,
+			Toast,
 		},
 	};
 });
