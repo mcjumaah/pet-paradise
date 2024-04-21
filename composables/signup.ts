407b593a3cd304fd67dc2d5b6c8cbe7ee63180a9
mcjumaah <87046678+mcjumaah@users.noreply.github.com>
2024-04-21 21:01:33 +0800

    chore(`signup` composable): Removed `useSignupCredentials`'s default values

--- composables/signup.ts
+++ composables/signup.ts
@@ -1,6 +1,6 @@
 export const useSignupCredentials = () => {
 	return useState<{ email: string; password: string }>("signup-credentials", () => {
-		return { email: "test@mail.com", password: "StrongPass123!" };
+		return { email: "", password: "" };
 	});
 };
 
