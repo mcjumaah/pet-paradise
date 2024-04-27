fd57697bd77900802258dae3ef3cb38b7a7b8326
mcjumaah <87046678+mcjumaah@users.noreply.github.com>
2024-04-27 16:57:32 +0800

    feat(Plugins): Added `useCurrentUserHelper`

---
+++ plugins/useCurrentUserHelper.ts
@@ -0,0 +1,39 @@
+import type { Cart } from "~/server/model/cart";
+import type { CustomerProjection } from "~/server/projections/customerProjections";
+
+export default defineNuxtPlugin(async () => {
+	const route = useRoute();
+
+	const { data: authData } = useAuth();
+
+	const { data: customerData } = await useFetch("/api/customer", {
+		method: "GET",
+		query: { email: authData.value?.user?.email },
+		transform: (_customerData: { data: CustomerProjection }) => _customerData.data,
+	});
+
+	const { data: cartData, execute } = await useFetch("/api/cart", {
+		method: "GET",
+		query: {
+			customerId: customerData.value?.id,
+		},
+		immediate: false,
+		transform: (_cartData: { data: Cart }) => _cartData.data,
+	});
+
+	await execute();
+
+	return {
+		provide: {
+			currentUserHelper: () => {
+				return {
+					userData: customerData.value,
+					cart: {
+						data: cartData.value,
+						fetch: execute,
+					},
+				};
+			},
+		},
+	};
+});
