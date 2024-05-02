e63e2f8360a5333027cc7f37d044ea959d11a779
mcjumaah <87046678+mcjumaah@users.noreply.github.com>
2024-05-02 13:54:18 +0800

    feat(`checkout` Composable): Added `CheckoutItem` type

--- composables/checkout.ts
+++ composables/checkout.ts
@@ -1,14 +1,19 @@
 import type { ProductItemProjection } from "~/server/projections/productItemProjections";
 
-export const useCheckoutItems = () => {
-	return useState<ProductItemProjection[]>("checkout-items", () => []);
+export type CheckoutItem = Omit<ProductItemProjection, "status"> & {
+	productItemId?: number;
+	origin: "cart" | "buy-now";
 };
 
-export function usePerItemTotalPrice(item: ProductItemProjection) {
+export const useCheckoutItems = () => {
+	return useState<CheckoutItem[]>("checkout-items", () => []);
+};
+
+export function usePerItemTotalPrice(item: ProductItemProjection | CheckoutItem) {
 	return item.price * item.quantity;
 }
 
-export function useItemsArrTotalPrice(items: ProductItemProjection[], idArr?: number[]) {
+export function useItemsArrTotalPrice(items: ProductItemProjection[] | CheckoutItem[], idArr?: number[]) {
 	let total = 0;
 	items.forEach((item) => {
 		if (!idArr || idArr.includes(item.id)) {
