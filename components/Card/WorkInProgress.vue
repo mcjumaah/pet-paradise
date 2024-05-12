e6a05951b0ea1272027777bda839cd31804eab3a
mcjumaah <87046678+mcjumaah@users.noreply.github.com>
2024-05-12 18:45:55 +0800

    Update WorkInProgress.vue

--- components/Card/WorkInProgress.vue
+++ components/Card/WorkInProgress.vue
@@ -2,7 +2,7 @@
 	<div
 		class="work-in-progress-placeholder d-flex flex-column w-100 justify-content-center align-items-center gap-2 text-muted"
 	>
-		<img class="work-in-progress-image opacity-75" src="/images/icons/work-in-progress.png" alt="Empty Cart" />
+		<img class="work-in-progress-image opacity-75" src="/images/icons/work-in-progress.png" alt="Work In Progress" />
 		<h4 class="mt-3 text-center">Sorry inconvenience but this section is still a work in progress. 😫😓</h4>
 		<h5 class="text-center">Thank you for understanding. 🙇</h5>
 	</div>
