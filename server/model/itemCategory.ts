85c14a0164e716a37121d1e037ca0095f9fd2a61
mcjumaah <87046678+mcjumaah@users.noreply.github.com>
2024-05-12 13:44:44 +0800

    feat(Entity Models): Added `itemCategory`

---
+++ server/model/itemCategory.ts
@@ -0,0 +1,100 @@
+import { sql } from "../db";
+import { keysToCamelCase } from "../utils/entityFieldsUtil";
+import { paginationSql } from "../utils/paginationUtil";
+
+export type ItemCategory = {
+	id: number;
+	name: string;
+};
+
+export type ItemCategoryDTO = Pick<ItemCategory, "name">;
+
+export type ItemCategoryPaginated = {
+	content: ItemCategory[];
+	pagination: Pagination;
+};
+
+export const findAll = async (pageNum: number = 0, search: string = "") => {
+	try {
+		search = `%${search}%`;
+		const { result, pagination } = await paginationSql(
+			pageNum,
+			`SELECT * FROM item_category WHERE (? = '%%' OR name LIKE ?)`,
+			[search, search]
+		);
+
+		return keysToCamelCase({
+			content: result as ItemCategory[],
+			pagination: pagination,
+		}) as ItemCategoryPaginated;
+	} catch (error) {
+		throw error;
+	}
+};
+
+export const findById = async (id: number) => {
+	try {
+		const result = keysToCamelCase(
+			await sql({
+				query: `SELECT * FROM item_category WHERE id = ?`,
+				values: [id],
+			})
+		) as ItemCategory[];
+
+		return result.length === 1 ? result[0] : null;
+	} catch (error) {
+		throw error;
+	}
+};
+
+export const save = async (data: ItemCategoryDTO) => {
+	try {
+		await sql({
+			query: `
+        INSERT INTO item_category (
+          name
+        ) VALUES (?)`,
+			values: [data.name],
+		});
+
+		const result = keysToCamelCase(
+			await sql({
+				query: `SELECT * FROM item_category WHERE id = LAST_INSERT_ID()`,
+			})
+		) as ItemCategory[];
+
+		return result.length === 1 ? result[0] : null;
+	} catch (error) {
+		throw error;
+	}
+};
+
+export const update = async (id: number, data: ItemCategoryDTO) => {
+	try {
+		await sql({
+			query: `
+      UPDATE item_category 
+      SET 
+        name = ? 
+      WHERE id = ?`,
+			values: [data.name, id],
+		});
+
+		return await findById(id);
+	} catch (error) {
+		throw error;
+	}
+};
+
+export const deleteById = async (id: number) => {
+	try {
+		await sql({
+			query: `DELETE FROM item_category WHERE id = ?`,
+			values: [id],
+		});
+
+		return true;
+	} catch (error) {
+		throw error;
+	}
+};
