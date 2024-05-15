392845ab757ed0fd70ec5e0653d483ad5d3f47d7
mcjumaah <87046678+mcjumaah@users.noreply.github.com>
2024-05-15 20:51:20 +0800

    feat(Admin Pages): Initialized admin pages

---
+++ pages/admin/index.vue
@@ -0,0 +1,15 @@
+<template>
+	<div class="admin-page"></div>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+	middleware: [
+		function () {
+			return navigateTo("/admin/products");
+		},
+	],
+});
+</script>
+
+<style scoped lang="scss"></style>
