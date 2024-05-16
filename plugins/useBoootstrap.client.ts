5e59384c5f74e61ae9756a7eff5ad97c2e6f34bf
mcjumaah <87046678+mcjumaah@users.noreply.github.com>
2024-05-16 10:46:42 +0800

    build(`useBoootstrap` Plugin): Added `Modal`

--- plugins/useBoootstrap.client.ts
+++ plugins/useBoootstrap.client.ts
@@ -1,4 +1,4 @@
-import { Dropdown, Collapse, Offcanvas, Tooltip, Alert, Toast } from "bootstrap";
+import { Dropdown, Collapse, Offcanvas, Tooltip, Alert, Toast, Modal } from "bootstrap";
 
 export default defineNuxtPlugin((nuxtApp) => {
 	nuxtApp.vueApp.mixin(Dropdown as any);
@@ -7,6 +7,7 @@ export default defineNuxtPlugin((nuxtApp) => {
 	nuxtApp.vueApp.mixin(Tooltip as any);
 	nuxtApp.vueApp.mixin(Toast as any);
 	nuxtApp.vueApp.mixin(Alert as any);
+	nuxtApp.vueApp.mixin(Modal as any);
 
 	return {
 		provide: {
